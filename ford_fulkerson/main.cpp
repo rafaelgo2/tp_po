95874c710e7a6488fe9b072af5625ade0a57f1ba
Rafael Grandsire <rafael.grandsire@gmail.com>
2017-06-15 18:12:01 -0300

    R

--- ford_fulkerson/main.cpp
+++ ford_fulkerson/main.cpp
@@ -4,18 +4,25 @@
 
 using namespace std;
 
+#define INF ( ((unsigned) -1) >> 1 )
+
+//CONFIG
+ifstream in("in.txt");
+//ifstream &in = cin;
+ostream &out = cout;
+
 struct Edge{
     int flow; //Quanto de flow ainda pode passar
     int capacity; //Capacidade da aresa
     int from; //Origem da aresta
     int to; //Destino da aresta
-    bool original; //Tipo da aresta (verdadeira ou falsa)
+    bool original; //Tipo da aresta (original ou residual)
     Edge(){}
     Edge(int capacity):
         capacity(capacity), flow(capacity), original(true){}
     Edge(int capacity, int from, int to):
             flow(capacity), capacity(capacity), from(from), to(to), original(true){}
-    Edge operator~(){
+    Edge operator~(){ //Transforma uma aresta original em residual
         Edge e(0, to, from);
         e.capacity = capacity;
         e.original = false;
@@ -25,37 +32,70 @@ struct Edge{
 
 typedef vector<int> Vertex;
 
-int dfs(int v, int d, int acc, vector<Vertex> &graph, vector<Edge> &e, vector<int> &ant, vector<bool> &path){
-    if (v == d) {
-        path[d] = true;
-        return acc;
+struct Graph {
+    vector<Vertex> v;
+    vector<Edge> e;
+    int n, m;
+    Graph(int n, int m):
+            n(n), m(m){
+        v.resize(n);
+        e.reserve(2*m);
     }
-    int aug;
-    for (int i = 0; i < graph[v].size(); i++){
-        Edge edge = e[graph[v][i]];
-        if (edge.flow > 0 && ant[edge.to] == -1) {
-            ant[edge.to] = v;
-            aug = dfs(edge.to, d, min(acc, edge.flow), graph, e, ant, path);
-            if (aug > 0){
-                if (e[graph[v][i]].original){
-                    path[graph[v][i]] = true;
-                    e[graph[v][i]].flow -= aug;
-                    e[graph[v][i] + e.size()/2].flow += aug;
+    void addEdge(Edge edge){
+        v[edge.from].push_back(e.size());
+        e.push_back(edge);
+        v[edge.to].push_back(e.size());
+        e.push_back(~edge);
+    }
+    int dfs(int curr, int dest, vector<int> &ant, vector<bool> &path, int acc){
+        if (curr == dest){
+            path[dest] = true;
+            return acc;
+        }
+        for (unsigned i = 0; i < v[curr].size(); i++){
+            Edge edge = e[v[curr][i]];
+            if (edge.flow > 0 && ant[edge.to] == -1){
+                ant[edge.to] = curr;
+                int aug = dfs(edge.to, dest, ant, path, min(acc, edge.flow));
+                if (aug > 0){
+                    int inc = edge.original ? 1 : -1;
+                    if (edge.original)
+                        path[v[curr][i]] = true;
+                    e[v[curr][i]+inc].flow += aug;
+                    e[v[curr][i]].flow -= aug;
+                    return aug;
                 }
-                else{
-                    e[graph[v][i] - e.size()/2].flow += aug;
-                    e[graph[v][i]].flow -= aug;
-                }
-                return aug;
             }
         }
+        return 0;
     }
-    return 0;
-}
+    int max_flow(int begin, int end){
+        vector<int> ant(n);
+        vector<bool> path(m);
+        int flow = 0;
+        while (true){
+            fill(ant.begin(), ant.end(), -1);
+            ant[begin] = begin;
+            fill(path.begin(), path.end(), false);
+            int aug = dfs(begin, end, ant, path, INF);
+            if (aug == 0)
+                break;
+            flow += aug;
+            for (unsigned i = 0; i < m; i++)
+                out << e[2*i].capacity - e[2*i].flow << " ";
+            out << endl;
+            for (unsigned i = 0; i < m; i++)
+                out << (int)path[i] << " ";
+            out << endl;
+            for (unsigned i = 0; i < m; i++)
+                out << e[2*i].capacity << " ";
+            out << endl << endl;
+        }
+        return flow;
+    }
+};
 
 int main() {
-    ifstream in("in.txt");
-    ostream &out = cout;
     int n, m, capacity;
     in >> n
        >> m;
@@ -77,37 +117,10 @@ int main() {
                 }break;
             }
         }
-    vector<Vertex> graph(n);
-    for (int i = 0; i < m; i++){
-        graph[e[i].from].push_back(i);
-        e.push_back(~e[i]);
-        graph[e[i].to].push_back(i+m);
-    }
-    vector<int> ant(n);
-    vector<bool> path(m);
-    int ans = 0;
-    while (true){
-        fill(ant.begin()+1, ant.end(), -1);
-        fill(path.begin(), path.end(), false);
-        int aug = dfs(0, n-1, INT32_MAX, graph, e, ant, path);
-        if (!aug)
-            break;
-        ans += aug;
-        out << "flow: " << endl;
-        for (int i = 0; i < m; i++)
-            out << e[i].capacity - e[i].flow << " ";
-        out << endl;
-        out << "path: " << endl;
-        for (int i = 0; i < m; i++)
-            out << (int)path[i] << " ";
-        out << endl;
-        out << "copy: " << endl;
-        for (int i = 0; i < m; i++)
-            out << e[i].capacity << " ";
-        out << endl;
-        out << ans << endl;
-        out << endl;
-    }
-    out << ans << endl;
+    Graph g(n, m);
+    for (unsigned i = 0; i < e.size(); i++)
+        g.addEdge(e[i]);
+    int flow = g.max_flow(0, n-1);
+    cout << "Max flow = " << flow << endl;
     return 0;
 }
